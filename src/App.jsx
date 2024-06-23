bc2be221e7d7dfd3868776ff63a3a9b21642038a
scorchy38 <vkumarsaraswat@gmail.com>
2024-06-23 05:49:43 +0530

    Integate posthog through npm package

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,30 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Intercom from '@intercom/messenger-js-sdk';
+import posthog from "posthog-js";
 // import Services from "./components/Services";
 
 function App() {
   Intercom({
     app_id: 'g8tbygyl',
   });
+  posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, { api_host: 'https://us.i.posthog.com', person_profiles: 'identified_only' })
 
-  return (
-  <>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Header />
-      <Hero />
-      <Benefits />
-      <Collaboration />
-      {/*<Services />*/}
-      <Pricing />
-      <Roadmap />
-      <Footer />
-    </div>
+    return (
+      <>
+        <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+          <Header/>
+          <Hero/>
+          <Benefits/>
+          <Collaboration/>
+          {/*<Services />*/}
+          <Pricing/>
+          <Roadmap/>
+          <Footer/>
+        </div>
 
-    <ButtonGradient />
-  </>
+        <ButtonGradient/>
+      </>
   )
 }
 
